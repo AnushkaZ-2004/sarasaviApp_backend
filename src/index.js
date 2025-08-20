5c5dcf34a2118bfdea14175809e5ff1ace156055
Anushka <anushkasahan200439@gmail.com>
2025-08-21 01:34:42 +0530

    Add new db.js file and update the index,js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("api/auth",authRoutes);
+app.use(express.json());
+
+// FIX: Added missing slash
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log('Server running on port ' + PORT);
-}); 
+    console.log("Server running on port " + PORT);
+    connectDB();
+});
